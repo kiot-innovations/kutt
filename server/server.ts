f14c2a62bc952805a9a4572eeeea4b72c64e0cde
Arihant <arihant.ekism@gmail.com>
2020-11-29 19:32:57 +0530

    Added docker-compose in gitignore

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,6 @@ const port = env.PORT;
 const app = nextApp({ dir: "./client", dev: env.isDev });
 const handle = app.getRequestHandler();
 
-
 app.prepare().then(async () => {
   const server = express();
 
